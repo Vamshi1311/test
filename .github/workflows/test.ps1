40f6a2c2b333db86f20d924d62f560f5bd7758a3
Vamshi1311 <98009378+Vamshi1311@users.noreply.github.com>
2024-03-26 13:17:25 +0530

    Update test.ps1

--- .github/workflows/test.ps1
+++ .github/workflows/test.ps1
@@ -1,1 +1,7 @@
+$devuser=$args[0]
+$devPword=$args[1]
 
+$User = "$SECRETS.$devuser"
+$PWord = ConvertTo-SecureString -String "$SECRETS.$devPword" -AsPlainText -Force
+$destinationCredentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
+Write-Output $destinationCredentials
